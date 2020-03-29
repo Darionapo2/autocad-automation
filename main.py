d67d13f7cda4fd5c3cc9fa77273ac0cccda4a763
dario <darionapo2.0@gmail.com>
2020-03-29 12:55:57 +0200

    Class SpotsBlock successful added

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from pyautocad import Autocad, APoint
 a_cad = Autocad(create_if_not_exists = True)
 
-def main():
-    insert_point = APoint(200, 5000)
-    generate_spots_block(insert_point, 140, 500, 87, double = True, border = 10, outline = True)
-
 def generate_rectangle(insert_point, width, height):
     a_cad.model.AddLine(insert_point, APoint(insert_point.x + width, insert_point.y))
     a_cad.model.AddLine(APoint(insert_point.x + width, insert_point.y), APoint(insert_point.x + width, insert_point.y + height))
@@ -16,25 +12,49 @@ def generate_spot(insert_point, width, height):
     a_cad.model.AddLine(APoint(insert_point.x + width, insert_point.y + height), APoint(insert_point.x, insert_point.y + height))
     a_cad.model.AddLine(APoint(insert_point.x, insert_point.y + height), insert_point)
 
-def generate_spots_block(insert_point, width, height, number, double = False, border = 0, outline = False):
-    for n in range(number):
-        if n == number - 1 or border != 0:
+def generate_single_spots_block(insert_point, width, height, spots_number, border = 0):
+    for n in range(spots_number):
+        if n == spots_number - 1 or border != 0:
             generate_rectangle(APoint(insert_point.x + ((width + border) * n), insert_point.y), width, height)
         else:
             generate_spot(APoint(insert_point.x + ((width + border) * n), insert_point.y), width, height)
 
-    if double:
-        generate_spots_block(APoint(insert_point.x, insert_point.y + height + border), width, height, number, border = border)
+class SpotsBlock:
+    def __init__(self, width, height, spots_number, double = False, border = 0, outline = False):
+        self.width = width
+        self.height = height
+        self.spots_number = spots_number
+        self.double = double
+        self.border = border
+        self.outline = outline
 
-    if outline:
-        rectangle_insert_point = APoint(insert_point.x - border, insert_point.y - border)
-        rectangle_width = (width + border) * number + border
-        rectangle_height = height + (2 * border) if not double else (2 * height) + (3 * border)
+    def draw(self, insert_point):
+        for n in range(self.spots_number):
+            if n == self.spots_number - 1 or self.border != 0:
+                generate_rectangle(APoint(insert_point.x + ((self.width + self.border) * n), insert_point.y), self.width, self.height)
+            else:
+                generate_spot(APoint(insert_point.x + ((self.width + self.border) * n), insert_point.y), self.width, self.height)
 
-        generate_rectangle(rectangle_insert_point, rectangle_width, rectangle_height)
+        if self.double:
+            upper_spots_block_insert_point = APoint(insert_point.x, insert_point.y + self.height + self.border)
 
-def add_transponder():
-    pass
+            generate_single_spots_block(upper_spots_block_insert_point, self.width, self.height, self.spots_number, self.border)
+
+        if self.outline:
+            rectangle_insert_point = APoint(insert_point.x - self.border, insert_point.y - self.border)
+            rectangle_width = (self.width + self.border) * self.spots_number + self.border
+            rectangle_height = self.height + (2 * self.border) if not self.double else (2 * self.height) + (3 * self.border)
+
+            generate_rectangle(rectangle_insert_point, rectangle_width, rectangle_height)
+
+    def add_transponders(self):
+        pass
+
+
+def main():
+    insert_point = APoint(200, 10000)
+    spots_block = SpotsBlock(140, 800, 100, double = True)
+    spots_block.draw(insert_point)
 
 if __name__ == '__main__':
     main()
